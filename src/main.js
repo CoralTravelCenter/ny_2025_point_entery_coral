407d9b32ed7ecd2b5c8acb90d6983ecbb289f7a4
Mike <mikhail-gankin@yandex.ru>
2024-12-09 10:52:32 +0300

    +++ hostReactReady

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {notAuth} from "./not-auth/not-auth.js";
 import {auth} from "./auth/auth.js";
-import {appendOnce} from "./usefuls.js";
+import {appendOnce, hostReactAppReady} from "./usefuls.js";
 import Cookies from 'js-cookie'
 import './not-auth/not-auth.scss'
 import './auth/auth.scss'
@@ -11,10 +11,14 @@ const placeToInsert = document.body
 const root = document.createElement('div');
 root.classList.add('ny_notauth_colba')
 
-if (Cookies.get('next-auth.session-token')) {
-	appendOnce(placeToInsert, auth(root, gamePage));
-	console.log('auth')
-} else {
-	appendOnce(placeToInsert, notAuth(root, gamePage));
-	console.log('not-auth')
-}
+hostReactAppReady().then(() => {
+	if (location.origin.includes('backoffice')) return;
+	if (Cookies.get('next-auth.session-token')) {
+		appendOnce(placeToInsert, auth(root, gamePage));
+		console.log('auth')
+	} else {
+		appendOnce(placeToInsert, notAuth(root, gamePage));
+		console.log('not-auth')
+	}
+})
+
